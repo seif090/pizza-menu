dc34771974de7a1a46815fb593ed734150169284
seif <seiftarek090@gmail.com>
2025-05-10 20:38:04 +0300

    Refactor CSS structure and integrate styles into the App component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
+
 
 const pizzaData = [
   {
@@ -47,7 +49,7 @@ const pizzaData = [
 ];
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,19 +59,19 @@ function App() {
 
 function Header(){
     return(
-        <header>
-            <h1>Fast React Pizza Co.</h1>
+        <header className="header">
+            <h1 style={{color:"red", fontSize:"40px", textAlign:"center"}}>Fast React Pizza Co.</h1>
         </header>
     );
 }
 function Menu(){
     return(
-        <div>
+        <main className="menu">
             <h2>Our Menu</h2>
             <Pizza />
             <Pizza />
             <Pizza />
-        </div>
+        </main>
     ); 
 }
 function Footer(){
@@ -83,7 +85,7 @@ function Footer(){
     }
     console.log(hour);
     return(
-        <footer>
+        <footer className="footer">
             {new Date().toLocateTimeString()}. We are currently open
         </footer>
     ); 
@@ -93,7 +95,7 @@ function Pizza(){
     return( 
     <div>
     <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"/>
-    <h2>Pizza Spinaci</h2>
+    <h3>Pizza Spinaci</h3>
     <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
     
